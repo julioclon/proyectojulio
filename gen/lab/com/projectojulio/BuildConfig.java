c563485116f6cf73abee47473baa12e32a5b909a
your Name <you@example.com>
2012-07-25 18:54:00 -0500

    subiendo

---
+++ gen/lab/com/projectojulio/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package lab.com.projectojulio;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/lab/com/projectojulio/MainActivity1.java
@@ -0,0 +1,30 @@
+package lab.com.projectojulio;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+import android.content.Intent;
+import android.view.*;
+
+public class MainActivity1 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_activity1, menu);
+        return true;
+    }
+
+    
+
+
+
+
+}
